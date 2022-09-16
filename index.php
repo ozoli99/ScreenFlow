90ea7798211a5f01f03c67157b0b062e02e8c905
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-16 19:43:06 +0200

    Implement navbar and add styling

--- index.php
+++ index.php
@@ -6,14 +6,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Orosz Zoltán">
 
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="stylesheet" href="css/styles.css"/>
+
         <title>Mainpage - ScreenFlow</title>
     </head>
-    <body>
-        <nav>
-
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top"><img src="assets/logo.ico">ScreenFlow</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="">Log In</a></li>
+                </ul>
+            </div>
         </nav>
         <header>
-            
+
         </header>
     </body>
 </html>
