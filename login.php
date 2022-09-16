1a30bf77a278ca527a5b8f33e87a319da7fb09a3
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-16 23:34:04 +0200

    Add server side validation to both login and signup

--- login.php
+++ login.php
@@ -1,3 +1,106 @@
+<?php
+    $data = [];
+    $errors = array_fill_keys(array("signupUsername", "signupEmail", "signupPassword", "signupPasswordAgain", "loginUsername", "loginPassword"), "");
+    $information = array_fill_keys(array("login", "signup"), "");
+    $is_login = false;
+    $has_user = false;
+    $active_user = [];
+
+    $json_string = file_get_contents("users.json");
+    $users = json_decode($json_string, true);
+
+    if (validate($_POST)) {
+        if (isset($_POST["signupSubmit"])) {
+            $is_login = false;
+            $new_data = [
+                "id" => $data["signupPassword"],
+                "username" => $data["signupUsername"],
+                "email" => $data["signupEmail"],
+                "login" => false,
+                "watched" => [
+                    "series1" => 0,
+                    "series2" => 0,
+                    "series3" => 0,
+                    "series4" => 0
+                ]
+            ];
+            array_push($users, $new_data);
+            $information["signup"] = "User successfully added";
+        }
+        if (isset($_POST["loginSubmit"])) {
+            if (is_array($users) || is_object($users)) {
+                foreach ($users as $key => $user) {
+                    if ($user["id"] === $data["loginPassword"] && $user["username"] === $data["loginUsername"]) {
+                        $users[$key]["login"] = true;
+                        $is_login = true;
+                        $has_user = true;
+                        $active_user = $users[$key];
+                        $information["login"] = "User successfully logged in";
+                    }
+                }
+            }
+            if (!$is_login) $information["login"] = "Wrong username or password";
+            else header("Location: ./index.php");
+        }
+    }
+    if (isset($_GET["logout"])) {
+        foreach ($users as $key => $user) {
+            if ($user["login"]) {
+                $users[$key]["login"] = false;
+                $has_user = false;
+                $active_user = [];
+            }
+        }
+    }
+
+    $new_array = json_encode($users, JSON_PRETTY_PRINT);
+    file_put_contents("users.json", $new_array);
+
+    function validate($fv) {
+        global $data, $errors;
+
+        $post_signup = filter_input(INPUT_POST, "signupSubmit");
+        $post_login = filter_input(INPUT_POST, "loginSubmit");
+
+        if (!empty($post_signup)) {
+            if ($fv["signupUsername"] === "") $errors["signupUsername"] = "Username is required";
+            else if (strlen(trim($fv["signupUsername"])) < 3) $errors["signupUsername"] = "Username length must be at least 3 characters long";
+            else if (!preg_match("/^[A-Za-z][A-Za-z0-9]{3,31}$/", $fv["signupUsername"])) $errors["signupUsername"] = "Username can only contain letters and whitespace";
+            else $data["signupUsername"] = $fv["signupUsername"];
+
+            if ($fv["signupEmail"] === "") $errors["signupEmail"] = "Email is required";
+            else if (!filter_var($fv["signupEmail"], FILTER_VALIDATE_EMAIL)) $errors["signupEmail"] = "Invalid email format";
+            else $data["signupEmail"] = $fv["signupEmail"];
+
+            if ($fv["signupPassword"] === "") $errors["signupPassword"] = "Password is required";
+            else if (!preg_match("/^[A-Za-z][A-Za-z0-9]{3,31}$/", $fv["signupPassword"])) $errors["signupPassword"] = "Invalid password format";
+            else $data["signupPassword"] = $fv["signupPassword"];
+
+            if ($fv["signupPasswordAgain"] === "") $errors["signupPasswordAgain"] = "You have to confirm your password";
+            else if ($fv["signupPassword"] !== $fv["signupPasswordAgain"]) $errors["signupPasswordAgain"] = "Passwords need to be the same";
+        }
+
+        if (!empty($post_login)) {
+            if ($fv["loginUsername"] === "") $errors["loginUsername"] = "Username is required";
+            else if (strlen(trim($fv["loginUsername"])) < 3) $errors["loginUsername"] = "Username length must be at least 3 characters long";
+            else if (!preg_match("/^[A-Za-z][A-Za-z0-9]{3,31}$/", $fv["loginUsername"])) $errors["loginUsername"] = "Username can only contain letters and whitespace";
+            else $data["loginUsername"] = $fv["loginUsername"];
+
+            if ($fv["loginPassword"] === "") $errors["loginPassword"] = "Password is required";
+            else if (!preg_match("/^[A-Za-z][A-Za-z0-9]{3,31}$/", $fv["loginPassword"])) $errors["loginPassword"] = "Invalid password format";
+            else $data["loginPassword"] = $fv["loginPassword"];
+        }
+
+        $is_empty = true;
+        foreach ($errors as $error => $error_value) {
+            if ($error_value !== "") {
+                $is_empty = false;
+            }
+        }
+
+        return $is_empty;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
