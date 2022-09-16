28f199b024baa504e11d3cecd14b9b5d2be32906
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-16 19:54:55 +0200

    Add Bootstrap Core JS

--- index.php
+++ index.php
@@ -64,5 +64,8 @@
                 </div>
             </div>
         </section>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
